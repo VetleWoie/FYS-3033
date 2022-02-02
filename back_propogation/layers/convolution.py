cb35112333d7a04a93f56e196c2f417c4dcd5e99
VetleWoie <vet.h.woie@hotmail.com>
2022-02-02 19:33:27 +0100

    Calculating output shape of convolutional layer

--- back_propogation/layers/convolution.py
+++ back_propogation/layers/convolution.py
@@ -2,18 +2,25 @@ from basic_layers import Layer
 import numpy as np
 
 class Conv2D(Layer):
-    def __init__(self, kernel_size,stride,num_kernels=1, learning_rate=0.1, padding='zeros') -> None:
+    def __init__(self, kernel_size,stride,num_kernels=1, learning_rate=0.1, padding = True, padding_type='zeros') -> None:
         self.kernel_size = kernel_size
         self.stride = stride
         self.num_kernels=num_kernels
         self.learning_rate=learning_rate
+        self.padding = padding
         self.padding_type = padding
         self.kernels = []
+
         for i in range(self.num_kernels):
             self.kernels.append(np.random.default_rng().uniform(low=-1, high=1,size=self.kernel_size))
 
-    def _init_weights(self, prev_outputs:int=None):
-        self.output_shape = None
+    def _init_weights(self, prev_outputs:int):
+        if self.padding:
+            #If padding is enabled then output shape will be equall to input shape
+            self.output_shape = (prev_outputs[0],prev_outputs[1],self.num_kernels)
+        else:
+            #If padding is disabled then output will be less than input
+            self.output_shape = (prev_outputs.shape[0]-self.kernel_size[0]//2, prev_outputs.shape[1]-self.kernel_size[1]//2, self.num_kernels)
 
     def evaluate(self, prev_output):
         raise NotImplementedError
