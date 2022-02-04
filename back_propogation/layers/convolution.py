6d795c9e4a00663b268d6b1ace0b28285b3e25f5
Vetle Hofsøy-Woie <vho023@uit.no>
2022-02-04 12:45:49 +0100

    Done with forward propogation for convoloution

--- back_propogation/layers/convolution.py
+++ back_propogation/layers/convolution.py
@@ -18,7 +18,7 @@ class Conv2D(Layer):
     def _init_weights(self, prev_outputs:int):
         if self.padding:
             #If padding is enabled then output shape will be equall to input shape
-            self.output_shape = (prev_outputs[0],prev_outputs[1],self.num_kernels)
+            self.output_shape = (self.num_kernels*prev_outputs[0],prev_outputs[1],prev_outputs[2])
         else:
             #If padding is disabled then output will be less than input
             self.output_shape = (prev_outputs.shape[0]-self.kernel_size[0]//2, prev_outputs.shape[1]-self.kernel_size[1]//2, self.num_kernels)
@@ -28,8 +28,9 @@ class Conv2D(Layer):
 
     def evaluate(self, prev_output):
         out = []
-        for input in prev_output:
-            out.append(convolve2d(input, self.kernels[0], mode='same'))
+        for kernel in self.kernels:
+            for input in prev_output:
+                out.append(convolve2d(input, kernel, mode='same'))
         return np.array(out)
     
     def calculate_error(self, label=None,cost=None,next_error = None, next_weights = None):
@@ -40,18 +41,19 @@ class Conv2D(Layer):
 
 if __name__=="__main__":
     np.set_printoptions(precision=2)
-    l = Conv2D((3,3),1, num_kernels=1)
+    l = Conv2D((3,3),1, num_kernels=2)
     for k in l.kernels:
         print(k)
 
     print("Original Image")
-    image = np.array([np.ones(shape=(5,5))]
-    )
+    image = np.array([np.ones(shape=(5,5))])#, np.ones(shape=(5,5))])
     l._init_weights(image.shape)
+    print(image.shape)
     print(image)
-
-    print("Padded Image")
-    padded_image = l.pad_zeros(image)
-    print(padded_image)
+    # print("Padded Image")
+    # padded_image = l.pad_zeros(image)
+    # print(padded_image)
     convolved_image = l.evaluate(image)
+    print(l.output_shape)
+    print(convolved_image.shape)
     print(convolved_image)
