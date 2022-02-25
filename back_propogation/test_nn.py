9cd741b1c08df775527a4715094c61423cdce9b2
Vetle Hofsøy-Woie <vho023@uit.no>
2022-02-25 11:06:25 +0100

    inital commit

--- back_propogation/test_nn.py
+++ back_propogation/test_nn.py
@@ -22,7 +22,6 @@ def draw_points(ax, set, labels, amount):
         ax.scatter(p[0],p[1],color = "blue" if l else "red")
 
 dataset = make_moons(n_samples=1000, noise = 0.1)
-
 nn = Neural_Net(2,[
     Input(2),
     Dense(3),
@@ -42,35 +41,44 @@ accuracy = []
 current_cost = 0
 cost = []
 
-batch_size = 100
-num_epochs = 400
+batch_size = 5
+num_epochs = 1000
 
 cf = Quadratic_cost()
 cost = []
 epochs = []
-for epoch in range(num_epochs):
-    ax[0].cla()
-    ax[1].cla()
-    ax[0].set_xlim([-2, 3])
-    ax[0].set_ylim([-2, 3])
 
-    print("Epoch: ", epoch)
-    correct = 0
-    out = nn.evaluate(dataset[0].T)
+nn.fit(num_epochs, batch_size,training=(dataset[0].T,np.expand_dims(dataset[1],0)))
 
-    cost.append(cf(out, np.expand_dims(dataset[1],0))[0,0]/len(dataset[0]))
-    epochs.append(epoch)
-    ax[1].plot(epochs, cost)
+# for epoch in range(num_epochs):
+#     # ax[0].cla()
+#     # ax[1].cla()
+#     # ax[0].set_xlim([-2, 3])
+#     # ax[0].set_ylim([-2, 3])
 
-    draw_points(ax[0], dataset[0], dataset[1], amount = 100)
-    draw_lines(ax[0], nn, 1)
-    plt.pause(0.1)
-    for i in range(0,len(dataset[0]),batch_size):
-        start = i
-        stop = start+batch_size
-        batch = dataset[0][start:stop]
-        batch_label = np.expand_dims(dataset[1][start:stop],0)
-        out = nn.evaluate(batch.T)
-        nn.back_propagate(batch_label)
-        nn.update_weights()
+#     print("Epoch: ", epoch)
+#     correct = 0
+#     out = nn.evaluate(dataset[0].T)
+
+#     cost.append(cf(out, np.expand_dims(dataset[1],0))[0,0]/len(dataset[0]))
+#     epochs.append(epoch)
+
+#     # draw_points(ax[0], dataset[0], dataset[1], amount = 100)
+#     # draw_lines(ax[0], nn, 1)
+#     # plt.pause(0.1)
+#     for i in range(0,len(dataset[0]),batch_size):
+#         start = i
+#         stop = start+batch_size
+#         batch = dataset[0][start:stop]
+#         batch_label = np.expand_dims(dataset[1][start:stop],0)
+#         out = nn.evaluate(batch.T)
+#         nn.back_propagate(batch_label)
+#         nn.update_weights()
+# ax[0].cla()
+# ax[1].cla()
+# ax[0].set_xlim([-2, 3])
+# ax[0].set_ylim([-2, 3])
+# ax[1].plot(epochs, cost)
+draw_points(ax[0], dataset[0], dataset[1], amount = 100)
+draw_lines(ax[0], nn, 1)
 plt.show()
