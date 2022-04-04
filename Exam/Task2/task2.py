9a377ee0d7d426084af41006e37a24647ca31524
Vetle Hofsøy-Woie <vho023@uit.no>
2022-04-04 18:28:53 +0200

    Work on task 2

--- Exam/Task2/task2.py
+++ Exam/Task2/task2.py
@@ -1,12 +1,10 @@
-from cProfile import label
-from re import I
 from unittest import result
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
-import tensorflow.keras.utils as tfutils
-
+from keras.applications.vgg16 import VGG16, decode_predictions, preprocess_input
+from keras import preprocessing
 PRECODEDIR = "problem2"
 TESTIAMGES = "test_images"
 VALIDATIONIMAGES = "validation_images"
@@ -33,71 +31,55 @@ def create_one_hot(val, max):
     onehot[val] = 1
     return onehot.reshape(1,-1)
 
-def load_validation_images(reshape_img = False, expected_shape = (224,224,3)):
+def load_validation_images(preprocess=False):
     x = []
     y = []
-    with open(f"{PRECODEDIR}/{SYNSETWORDS}") as file:
-        num_classes = len(file.readlines())
+    with open(f"{PRECODEDIR}/{SYNSETWORDS}") as synset_file:
+        words = synset_file.readlines()
     with open(f"{PRECODEDIR}/{VALIDATIONFILES}", 'r') as file:
         filenames = file.readlines()
-        
         print(f"{PRECODEDIR}/{VALIDATIONIMAGES}/{filenames[0].split(' ')[0]}")
         for filename in filenames:
             filename, c = filename.split(" ")
-            img = Image.open(f"{PRECODEDIR}/{VALIDATIONIMAGES}/{filename}")
-            if reshape_img:
-                img = isotropic_reshape(img, expected_shape)
-            img = np.array(img)/255
-            if reshape_img:
-                #Crop largest axis to wanted size
-                if img.shape[0] > img.shape[1]:
-                    img = img[:expected_shape[0], :]
-                else:
-                    img = img[:,:expected_shape[1]]
-            
+            img = np.array(preprocessing.image.load_img(f"{PRECODEDIR}/{VALIDATIONIMAGES}/{filename}"))#,target_size=(224,224)))
+            img = preprocessing.image.smart_resize(img, (224,224))
+            if preprocess:
+                img = preprocess_input(img)
             x.append(img)
-            y.append(int(c))
-    return x,tfutils.to_categorical(y, num_classes)
+            y.append((int(c), words[int(c)].split()[0]))
+    return np.asarray(x),np.array(y)
+    
+def load_synset_words():
+    ids = []
+    word_dict = {}
+    with open(f"{PRECODEDIR}/{SYNSETWORDS}") as file:
+        words = file.readlines()
+    for word in words:
+        word = word.split(" ")
+        id = word[0]
+        ids.append(id)
+        word_dict[id] = word[1:]
+    return ids, word_dict
 
-def load_validation_images_from_dir(dir):
-    labels = []
-    with open(f"./{PRECODEDIR}/{VALIDATIONFILES}", 'r') as file:
-        filenames = file.readlines()
-        for filename in filenames:
-            filename, c = filename.split(" ")
-            labels.append(int(c))
-    print(labels)
-
-    x = tfutils.image_dataset_from_directory(directory=dir, 
-                                        labels=labels,
-                                        label_mode="categorical",
-                                        color_mode="rgb",
-                                        crop_to_aspect_ratio=True,
-                                        image_size=(224,224))
-    print(x)
-
+        
 def show_validation_images(images, classes, duration = 0.3):
     fig, ax = plt.subplots(1,1)
     for img, c in zip(images, classes):
-        i = np.where(c == 1)[0][0]
-        print(i)
         try:
-            ax.imshow(img)
-            ax.set_title(f"Class: {i}, shape = {img.shape}")
+            ax.imshow(img[0])
+            ax.set_title(f"Class: {c}, shape = {img.shape}")
             plt.pause(duration)
         except Exception as e:
             print(e)
             plt.close(fig)
             break
+    plt.close(fig)
 
 if __name__ == "__main__":
-    # images, classes = load_validation_images(reshape_img=True)
-    load_validation_images_from_dir(f"./{PRECODEDIR}/test")
-    exit()
+    images, labels = load_validation_images(preprocess=True)
+    ids, word_dir = load_synset_words()
 
-    # show_validation_images(images, classes)
-    print("Image shape:",images[0].shape)
-    # print(images)
+    # show_validation_images(images, labels)
 
     vgg16 = tf.keras.applications.vgg16.VGG16(
     include_top=True,
@@ -105,20 +87,15 @@ if __name__ == "__main__":
     input_tensor=None,
     input_shape=None,
     pooling=None,
-    classes=len(classes[0]),
+    classes=1000,
     classifier_activation='softmax')
 
     vgg16.compile()
 
     print(vgg16.summary())
 
-    print(len(images), len(classes))
-    print(classes[0].shape)
-    print("Images shape",images.shape)
-    res = vgg16.evaluate(images[:1], classes[:1])
-    print("Loss, Acc", res)
-
-    # for img, c in zip(images[:1], classes[:1]):
-    #     img = img.reshape(1,224,224,3)
-    #     print(img.shape, c.T.shape)
-    #     print(vgg16.predict(img).shape)
+    pred = vgg16.predict(images)
+    pred = np.array(decode_predictions(pred, top=1)).reshape(95,3)
+    correct = (pred[:,0] == labels[:,1])
+    print(f"Accuracy: {correct.sum()}/{len(correct)} = {correct.sum()/len(correct)}")
+    exit()
