2d151330950fb10aa2dfeebe01b2470dcc4972d6
Vetle Hofsøy-Woie <vet.h.woie@hotmail.com>
2022-04-26 16:43:25 +0200

    task3 b done?

--- Exam/Task3/task3.py
+++ Exam/Task3/task3.py
@@ -124,16 +124,18 @@ def permutation_vector_to_class_vector(vectors):
     return tf.keras.utils.to_categorical(idxs, num_classes=24)
 
 def axis_acc(y_true, y_pred):
+    label = y_true
+    guess = y_pred
     perm = Permutation(4)
-    print(y_true.shape)
-    print(y_pred.shape)
-    y_true = backend.argmax(y_true)
-    y_pred = backend.argmax(y_pred)
-    x = []
-    for y in y_true:
-        x.append(y)
-    print(x)
-    return np.mean(x)
+    label = np.argmax(label,axis=1)
+    guess = np.argmax(guess,axis=1)
+    sum = 0
+    for i in range(label.shape[0]):
+        l = np.array(perm[label[i]])
+        g = np.array(perm[guess[i]])
+        sum += np.sum(l == g)
+
+    return sum / (len(label)*4)
 
 if __name__ == "__main__":
     x_tr, y_tr, x_te, y_te = load_data(f"{DATAFOLDER}/{DATASHUFFLED}")
@@ -141,18 +143,6 @@ if __name__ == "__main__":
     x_tr_p=create_patched_dataset(x_tr)
     x_te_p=create_patched_dataset(x_te)
     
-    # model = create_model(batch_norm=False, dropout=False, l2=False)
-    # model_bn = create_model(batch_norm=True, dropout=False, l2=False)
-    # model_do = create_model(batch_norm=False, dropout=True, l2=False)
-    # model_bnl2 = create_model(batch_norm=True, dropout=False, l2=True)
-    # model_bndol2 = create_model(batch_norm=True, dropout=True, l2=True)
-
-    # dt_model = create_time_distributed_model(batch_norm=False, dropout=False, l2=False)
-    # dt_model_bn = create_time_distributed_model(batch_norm=True, dropout=False, l2=False)
-    # dt_model_do = create_time_distributed_model(batch_norm=False, dropout=True, l2=False)
-    # dt_model_bnl2 = create_time_distributed_model(batch_norm=True, dropout=False, l2=True)
-    # dt_model_bndol2 = create_time_distributed_model(batch_norm=True, dropout=True, l2=True)
-
     model_list = [(0,{"batch_norm":False, "dropout":False,"l2":False},"VGG 11"),
                  (0,{"batch_norm":True, "dropout":False,"l2":False},"VGG 11 with batchnorm"),
                  (0,{"batch_norm":False, "dropout":True,"l2":False},"VGG 11 with dropout"),
@@ -173,7 +163,7 @@ if __name__ == "__main__":
         m = create_model(batch_norm=m_arg["batch_norm"], dropout=m_arg["dropout"], l2=m_arg["l2"])
     m.compile(loss="categorical_crossentropy",
                     optimizer="adam",
-                    metrics=["acc"],
+                    metrics=["acc",axis_acc],
                     run_eagerly=True)
 
 
@@ -183,19 +173,24 @@ if __name__ == "__main__":
                         y=y_tr,
                         validation_data=(x_te if not dt else x_te_p, y_te), 
                         batch_size=batch_size, epochs=100)
-    fig, ax = plt.subplots(1,2)
+    fig, ax = plt.subplots(1,3)
     fig.suptitle(title)
     ax[0].set_title("Loss")
-    ax[0].plot(history.history["loss"],label="Trainging")
+    ax[0].plot(history.history["loss"],label="Training")
     ax[0].plot(history.history["val_loss"],label="Validation")
     ax[0].set_xlabel("Epochs")
     ax[0].set_ylabel("Loss")
     ax[1].set_title("Image accuracy")
-    ax[1].plot(history.history["acc"],label="Trainging")
+    ax[1].plot(history.history["acc"],label="Training")
     ax[1].plot(history.history["val_acc"],label="Validation")
     ax[1].set_xlabel("Epochs")
-    ax[1].set_ylabel("Image Accuracy")
+    ax[1].set_ylabel("Image Accuracy(%)")
     ax[1].plot(np.full(len(history.history["acc"]),0.6))
+    ax[2].set_title("Quadrant accuracy")
+    ax[2].plot(history.history["axis_acc"],label="Training")
+    ax[2].plot(history.history["val_axis_acc"],label="Validation")
+    ax[2].set_xlabel("Epochs")
+    ax[2].set_ylabel("Quadrant Accuracy(%)")
     fig.tight_layout()
     plt.legend()
     filename = title.replace(" ", "")
